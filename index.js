b8ab87d04a0821df44a18ec225865b1e9f0e096c
RSandiford <rodolfo.sandiford@utp.ac.pa>
2021-07-31 19:24:09 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,34 @@
+/*Laboratorio 2 */
+
+//Segunda parte
+var a = 7;
+var b = 3;
+
+console.log(`Suma: ${a} + ${b} = ${a + b}`);
+console.log(`Resta: ${a} - ${b} = ${a - b}`);
+console.log(`Multiplicación: ${a} * ${b} = ${a * b}`);
+console.log(`División: ${a} / ${b} = ${a / b}`);
+
+//Tercera parte
+let texto1 = "Uniendo";
+let texto2 = "Textos";
+
+console.log(texto1.concat(" "+texto2));
+
+//Cuarta parte
+const num1 = 38;
+const num2 = "Lab2";
+
+console.log(typeof(num1));
+console.log(typeof(num2));
+
+//Quinta parte
+const obj = {
+	num: 38,
+	text: "Laboratorio 2",
+	salida: true,
+	vacio:[]
+}
+
+console.log(obj);
+
